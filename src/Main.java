7c5977758a957968958913f9783350868ee41294
Lbensimo <leofacebook42@orange.fr>
2023-04-25 16:59:59 +0200

    TP 2 terminé

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.io.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        read("src/fichier.txt");
+    }
+
+    public static void read(String path) {
+        try {
+            File myObj = new File(path);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    public static void print2() {
+        String content = "Ceci est le contenu ajouté en plus test";
+
+        File myObj = new File("src/fichier.txt"); // Specify the filename
+
+        try {
+            PrintWriter myWriter = new PrintWriter(new FileWriter(myObj, true));
+            myWriter.write(content);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    public static void print() {
+        String content = "Ceci est le contenu ajouté en plus";
+
+        File myObj = new File("src/fichier.txt"); // Specify the filename
+
+        try {
+            FileWriter myWriter = new FileWriter(myObj);
+            myWriter.write(content);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
